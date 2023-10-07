5ad348eca8b3d5a490c0fcac901432b980700007
ayuba pelumi <132446974+adebart@users.noreply.github.com>
2023-10-07 01:26:12 +0100

    added javascript

---
+++ main.js
@@ -0,0 +1,96 @@
+function mode(){
+    document.querySelector('body').style.backgroundColor = "white"
+    document.querySelector('.div2').style.color = "black"
+    document.querySelector('.img1').style.borderColor = 'black'
+    document.querySelector('.pow').style.color = "black"
+    document.querySelector('.h13').style.color = "black"
+    document.querySelector('.popp').style.color = "black"
+    document.querySelector('.grids').style.backgroundColor = "black"
+    document.querySelector('#see').style.backgroundColor = "black"
+    document.querySelector('#see1').style.backgroundColor = "black"
+    document.querySelector('#see2').style.backgroundColor = "black"
+    document.querySelector('#see3').style.backgroundColor = "black"
+    document.querySelector('#see4').style.backgroundColor = "black"
+   document.getElementById('sww').style.color = 'white'
+   document.getElementById('swws').style.color = 'white'
+   document.getElementById('swwss').style.color = 'white'
+   document.getElementById('swww').style.color = 'white'
+   document.getElementById('swwq').style.color = 'white'
+   document.getElementById('swwa').style.color = 'white'
+   document.getElementById('swwz').style.color = 'white'
+   document.getElementById('swwx').style.color = 'white'
+   document.getElementById('swwf').style.color = 'white'
+   document.getElementById('swwff').style.color = 'white'
+   document.getElementById('swwe').style.color = 'white'
+   document.getElementById('swwee').style.color = 'white'
+   document.querySelector('.h4').style.color = 'black'
+   document.querySelector('.aaa').style.color = 'black'
+   document.querySelector('.asaa').style.color = 'black'
+//    document.querySelector('.h4').style.color = 'black'
+   document.querySelector('.but5').style.display = 'none'
+   document.querySelector('.but55').style.display = 'block'
+   document.querySelector('#swwr').style.backgroundColor = "black"
+   document.querySelector('.a1').style.color = "white"
+   document.querySelector('#gg').style.color = "white"
+   document.querySelector('#go').style.color = "white"
+   document.querySelector('#op').style.color = "white"
+   document.querySelector('#jj').style.color = "white"
+   document.querySelector('.but55').style.color = "white"
+   document.querySelector('.but55').style.backgroundColor = "black"
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+}
+function mode1(){
+    document.querySelector('body').style.backgroundColor = "black"
+    document.querySelector('.div2').style.color = "white"
+    // document.querySelector('img1').style.border = "solidBlack"
+    document.querySelector('.pow').style.color = "white"
+    document.querySelector('.h13').style.color = "white"
+    document.querySelector('.popp').style.color = "white"
+    document.querySelector('.grids').style.backgroundColor = "white"
+    document.querySelector('#see').style.backgroundColor = "white"
+    document.querySelector('#see1').style.backgroundColor = "white"
+    document.querySelector('#see2').style.backgroundColor = "white"
+    document.querySelector('#see3').style.backgroundColor = "white"
+    document.querySelector('#see4').style.backgroundColor = "white"
+   document.getElementById('sww').style.color = 'black'
+   document.getElementById('swws').style.color = 'black'
+   document.getElementById('swwss').style.color = 'black'
+   document.getElementById('swww').style.color = 'black'
+   document.getElementById('swwq').style.color = 'black'
+   document.getElementById('swwa').style.color = 'black'
+   document.getElementById('swwz').style.color = 'black'
+   document.getElementById('swwx').style.color = 'black'
+   document.getElementById('swwf').style.color = 'black'
+   document.getElementById('swwff').style.color = 'black'
+   document.getElementById('swwe').style.color = 'black'
+   document.getElementById('swwee').style.color = 'black'
+   document.querySelector('.h4').style.color = 'white'
+   document.querySelector('.aaa').style.color = 'white'
+   document.querySelector('.asaa').style.color = 'white'
+//    document.querySelector('.h4').style.color = 'black'
+   document.querySelector('.but5').style.display = 'block'
+   document.querySelector('.but55').style.display = 'none'
+    document.querySelector('#see2').style.backgroundColor = "white"
+    document.querySelector('#swwr').style.backgroundColor = "white"
+    document.querySelector('.a1').style.color = "black"
+    document.querySelector('#gg').style.color = "black"
+    document.querySelector('#go').style.color = "black"
+    document.querySelector('#op').style.color = "black"
+    document.querySelector('#jj').style.color = "black"
+    document.querySelector('.img1').style.borderColor = 'white'
+
+
+}
